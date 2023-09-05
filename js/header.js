b63329c1fcc397599b2b77b2377bf12944fcbfe4
DominikR02 <125642128+DominikR02@users.noreply.github.com>
2023-09-05 12:05:15 +0200

    css & js
    css & js vom header wurden ausgelagert in eigene Datein

---
+++ js/header.js
@@ -0,0 +1,25 @@
+// JavaScript-Funktion zum Ein- und Ausblenden des Pop-up-Menüs
+function toggleBurgerMenu() {
+    var popupMenu = document.getElementById('popupMenu');
+    var popupOverlay = document.getElementById('popupOverlay');
+
+    // Wenn das Pop-up-Menü ausgeblendet ist, öffne es und zeige das Overlay
+    if (popupMenu.style.right === '-300px' || popupMenu.style.right === '') {
+        popupMenu.style.right = '0';
+        popupOverlay.style.display = 'block';
+    } else {
+        // Andernfalls, wenn das Pop-up-Menü sichtbar ist, schließe es und verberge das Overlay
+        popupMenu.style.right = '-300px';
+        popupOverlay.style.display = 'none';
+    }
+}
+
+// JavaScript-Funktion zum Schließen des Pop-up-Menüs
+function closePopupMenu() {
+    var popupMenu = document.getElementById('popupMenu');
+    var popupOverlay = document.getElementById('popupOverlay');
+
+    // Schließe das Pop-up-Menü und verberge das Overlay
+    popupMenu.style.right = '-300px';
+    popupOverlay.style.display = 'none';
+}
