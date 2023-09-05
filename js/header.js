e720d3b4e2d76cc21c9a48608af66a801d52be67
DominikR02 <125642128+DominikR02@users.noreply.github.com>
2023-09-05 13:45:08 +0200

    Search PopUp
    Hinzufügen des SearchPopUp-Fenster. Noch nicht vollständig befüllt

--- js/header.js
+++ js/header.js
@@ -24,3 +24,28 @@ function closePopupMenu() {
     popupOverlay.style.display = 'none';
 }
 
+// JavaScript-Funktion zum Ein- und Ausblenden des Pop-up-Menüs
+function toggleSearchMenu() {
+    var popupMenu = document.getElementById('popupMenuSearch');
+    var popupOverlay = document.getElementById('popupOverlaySearch');
+
+    // Wenn das Pop-up-Menü ausgeblendet ist, öffne es und zeige das Overlay
+    if (popupMenu.style.right === '-300px' || popupMenu.style.right === '') {
+        popupMenu.style.right = '0';
+        popupOverlay.style.display = 'block';
+    } else {
+        // Andernfalls, wenn das Pop-up-Menü sichtbar ist, schließe es und verberge das Overlay
+        popupMenu.style.right = '-300px';
+        popupOverlay.style.display = 'none';
+    }
+}
+
+// JavaScript-Funktion zum Schließen des Pop-up-Menüs
+function closePopupMenuSearch() {
+    var popupMenu = document.getElementById('popupMenuSearch');
+    var popupOverlay = document.getElementById('popupOverlaySearch');
+
+    // Schließe das Pop-up-Menü und verberge das Overlay
+    popupMenu.style.right = '-300px';
+    popupOverlay.style.display = 'none';
+}
